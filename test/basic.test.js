1a5ab788c5b77174a5461a613b69ea7d95eec367
Ivo Georgiev <ivo@linvo.org>
2015-05-19 21:01:19 +0300

    subtitle retriever + added one test

--- test/basic.test.js
+++ test/basic.test.js
@@ -5,6 +5,7 @@ var fs = require("fs");
 
 var parseSrt = require("../lib/srt").parseString;
 var heatmap = require("../lib/heatmap");
+var retrieve = require("../lib/retriever").retrieveSrt;
 
 describe("srt", function() {
 	it("parse srt file to retrieve the timestamps", function(done) {
@@ -33,9 +34,32 @@ describe("heatmap", function() {
 
 });
 
+// http://www.yifysubtitles.com/subtitle-api/american-psycho-yify-10962.zip
+// http://dl.opensubtitles.org/en/download/filead/src-api/vrf-07a4118f99/sid-hde04ngpuvva7nqkddrukas676/1952452279.gz
 describe("retriever", function() {
+	it("retrieve srt (local file)", function(done) {
+		// TODO
+		done();
+	});
 
+	it("retrieve srt from zip", function(done) {
+		retrieve("http://www.yifysubtitles.com/subtitle-api/american-psycho-yify-10962.zip", function(err, sub) {
+			assert.isNull(err);
+			assert.isDefined(sub);
+			assert.isTrue(typeof(sub)=="string");
+			done();
+		})
+	});
 
+	it("retrieve srt from gz", function(done) {
+		// TODO
+		done();
+	});
+
+	it("retrieve srt from gz (local file)", function(done) {
+		// TODO
+		done();
+	});
 });
 
 describe("grouping", function() {
